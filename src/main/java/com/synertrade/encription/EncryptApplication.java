58a1d38aa79f70dc9732268e08d545f06d125c9e
AndreyShchagin <andrey.shchagin@synertrade.com>
2017-02-16 11:04:38 +0100

    AES_PoC: Initial commit

---
+++ src/main/java/com/synertrade/encription/EncryptApplication.java
@@ -0,0 +1,12 @@
+package com.synertrade.encription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptApplication.class, args);
+	}
+}
